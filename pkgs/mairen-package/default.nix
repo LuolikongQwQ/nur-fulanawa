c352f7b929f0230072c7de68046aa3539d01f1ed
LuolikongQwQ <qq7731660@gmail.com>
2024-06-05 23:29:22 +0800

    add packages test

---
+++ pkgs/mairen-package/default.nix
@@ -0,0 +1,15 @@
+{ setdev }:
+setenv.mkDerivation rec {
+  name = "mairen-${version}";
+  version = "0.0.1";
+  src = ./.;
+  nativeBuildInputs = [ zstd ];
+  unpackPhase = ''
+    zstd -d $src/mairen-bin-2.0.0-1-any.pkg.tar.zst
+    tar -xvf $src/mairen-bin-2.0.0-1-any.pkg.tar
+  '';
+  installPhase = ''
+    install -Dm744 $src/usr/bin/* $out
+  '';
+}
+
